60c9e6a79f6cd84e8610cac683fb346337335752
ChibiFR <23081995b@gmail.com>
2017-08-29 18:19:41 +0200

    Add types declaration file

---
+++ typings/index.d.ts
@@ -0,0 +1,8 @@
+declare module 'ziptool' {
+  export type ziptoolCallback = (err: Error) => void;
+
+  export const VERSION: string;
+
+  export function zip(src: string|string, dest: string, callback: ziptoolCallback): void;
+  export function unzip(src: string, dest: string, callback: ziptoolCallback): void;
+}
